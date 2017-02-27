664fa41bc6b2751c2ee8208a53ccf2b139ddb03b
Robin Munn <robin_munn@sil.org>
2017-02-27 11:53:14 +0700

    Initial commit from ProjectScaffold

---
+++ tests/ReproCsv.Tests/Tests.fs
@@ -0,0 +1,10 @@
+module ReproCsv.Tests
+
+open ReproCsv
+open NUnit.Framework
+
+[<Test>]
+let ``hello returns 42`` () =
+  let result = Library.hello 42
+  printfn "%i" result
+  Assert.AreEqual(42,result)
